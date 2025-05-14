2ff82365b9fa533211276beba962df5d6df27436
ben <ben@box>
2025-05-14 00:00:33 -0500

    tidy

--- profiles/benchmacs/init.el
+++ profiles/benchmacs/init.el
@@ -1,3 +1,15 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 (add-to-list 'load-path "~/.emacs.d/c0r/")
 (require 'capture)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
